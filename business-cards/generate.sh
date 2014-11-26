8a53928c9d22afb200fafa5c8d0757e3119ce02f
Mitar <mitar.git@tnode.com>
2014-11-25 22:08:14 -0800

    Script for generating PDFs.

---
+++ business-cards/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+if [[ $# -ne 3 ]]; then
+	echo "Usage: $0 <first name> <last name> <email prefix>";
+	exit 1;
+fi
+
+FIRSTNAME="$1"
+LASTNAME="$2"
+EMAIL="$3"
+
+sed -e "s/FIRSTNAME/$FIRSTNAME/g" peerlibrary_card_front-01.svg | sed -e "s/LASTNAME/$LASTNAME/g" | sed -e "s/EMAIL/$EMAIL/g" > front.svg
+
+/Applications/Inkscape.app/Contents/Resources/bin/inkscape --without-gui --export-pdf="$EMAIL.pdf" front.svg
+/Applications/Inkscape.app/Contents/Resources/bin/inkscape --without-gui --export-pdf="back.pdf" peerlibrary_card_back-02.svg
+
+rm -f front.svg
