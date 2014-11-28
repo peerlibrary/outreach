3e18e0dac42d1889e73303efde7f30e41521fd63
Mitar <mitar.git@tnode.com>
2014-11-28 02:45:55 -0800

    Updated cards to new version with thicker fonts.

--- business-cards/generate.sh
+++ business-cards/generate.sh
@@ -9,9 +9,9 @@ FIRSTNAME="$1"
 LASTNAME="$2"
 EMAIL="$3"
 
-sed -e "s/FIRSTNAME/$FIRSTNAME/g" peerlibrary_card_front-01.svg | sed -e "s/LASTNAME/$LASTNAME/g" | sed -e "s/EMAIL/$EMAIL/g" > front.svg
+sed -e "s/FIRSTNAME/$FIRSTNAME/g" peerlibrary_card_front.svg | sed -e "s/LASTNAME/$LASTNAME/g" | sed -e "s/EMAIL/$EMAIL/g" > front.svg
 
 /Applications/Inkscape.app/Contents/Resources/bin/inkscape --without-gui --export-pdf="$EMAIL.pdf" front.svg
-/Applications/Inkscape.app/Contents/Resources/bin/inkscape --without-gui --export-pdf="back.pdf" peerlibrary_card_back-02.svg
+/Applications/Inkscape.app/Contents/Resources/bin/inkscape --without-gui --export-pdf="back.pdf" peerlibrary_card_back.svg
 
 rm -f front.svg
